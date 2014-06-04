e3c367eeb50456ba3fab43e22663c530a7820948
Daniel Margol <daniel.margol@gmail.com>
2014-06-04 19:33:46 -0400

    Text field added and able to submit any text. The text submitted is then sent from Angular to Express and then checked with google maps geocoder. The returned geocode data is then displayed with console.log in the terminal.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,22 @@
 
 angular.module('morningNinjaApp')
   .controller('MainCtrl', function ($scope, $http) {
+
     $http.get('/api/awesomeThings').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
     });
+
+    $scope.latAndLong = [];
+
+    $scope.zipCode = {userZip: ''};
+
+    $scope.submitZip = function() {
+      $scope.latAndLong.push($scope.zipCode);
+
+      $http.post('/api/awesomeThings', $scope.zipCode).success(function(err, awesomeThings) {
+        $scope.awesomeThings = awesomeThings;
+      });
+
+      $scope.zipCode = {userZip: ''};
+    };
   });
